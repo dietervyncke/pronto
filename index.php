bad2d88c1c4e76566554028beb0075479320af7b
Rein Van Oyen <reinvanoyen@gmail.com>
2018-12-17 15:35:44 +0100

    -d option added on bin for writeDir

--- index.php
+++ index.php
@@ -6,48 +6,45 @@ require_once 'util/debug.inc.php';
 require_once 'vendor/autoload.php';
 
 // parse CLI options
-
 $opts = getopt('ai:o:s:d:', [], $lastIndex );
 
 $interactiveShell = (isset($opts['a']) ? true : false);
-$inputPath = ( isset( $opts[ 'i' ] ) ? $opts[ 'i' ] : NULL );
-$outputPath = ( isset( $opts[ 'o' ] ) ? $opts[ 'o' ] : NULL );
+$inputFile = ( isset( $opts[ 'i' ] ) ? $opts[ 'i' ] : NULL );
+$outputFile = ( isset( $opts[ 'o' ] ) ? $opts[ 'o' ] : NULL );
 $inputSource = ( isset( $opts[ 's' ] ) ? $opts[ 's' ] : NULL );
 $writeDir = ( isset( $opts[ 'd' ] ) ? $opts[ 'd' ] : NULL );
 
-
 $cwd = getcwd();
-$runPath = ( dirname( getcwd() . '/' . $inputPath ) );
+$runPath = (dirname(getcwd().'/'.$inputFile));
+$writePath = (getcwd().'/'.$writeDir);
 
 if ($interactiveShell) {
-	$shell = new \Pronto\Console\InteractiveShell(new \Pronto\Input\ConsoleInput(), $cwd, $runPath);
+	$shell = new \Pronto\Console\InteractiveShell(new \Pronto\Input\ConsoleInput(), $cwd, $runPath, $writePath);
 	$shell->start();
 }
 
-$source = ( $inputPath ? file_get_contents( getcwd() . '/' . $inputPath ) : $inputSource );
+$source = ($inputFile ? file_get_contents(getcwd().'/'.$inputFile) : $inputSource);
 
 // lex
 $lexer = new \Pronto\Lexer();
-$tokens = $lexer->tokenize( $source );
+$tokens = $lexer->tokenize($source);
 
 // parse
-$parser = new \Pronto\Parser( $tokens );
+$parser = new \Pronto\Parser($tokens);
 $ast = $parser->parse();
 
 // compile
 $compiler = new \Pronto\Compiler();
-$code = $compiler->compile( $ast );
+$code = $compiler->compile($ast);
 
 // execute the compiled code
-
-$output = ($outputPath ? new \Pronto\Output\FileOutput($outputPath) : new \Pronto\Output\ConsoleOutput());
+$output = ($outputFile ? new \Pronto\Output\FileOutput($outputFile) : new \Pronto\Output\ConsoleOutput());
 $input = new \Pronto\Input\ConsoleInput();
 
 // create a new runtime and buffer and pass them to the environment
-
 $runtime = new \Pronto\Runtime();
 $buffer = new \Pronto\Buffer\DefaultBuffer();
-$environment = new \Pronto\Environment($runtime, $buffer, $output, $input, $cwd, $runPath);
+$environment = new \Pronto\Environment($runtime, $buffer, $output, $input, $cwd, $runPath, $writePath);
 
 // execute the compiled code
 $environment->execute($code);
